9e9759ffdbc78d4066a45d767c4635b43d315c40
MostakemHossain <mostakememon123@gmail.com>
2024-04-03 15:09:07 +0600

    Header complete

---
+++ app/_components/Header.jsx
@@ -0,0 +1,31 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs"
+import { Search } from "lucide-react"
+import Image from "next/image"
+
+function Header() {
+    const { user, isSignedIn } = useUser()
+    return (
+        <div className="flex justify-between items-center p-6 md:px-20 shadow-md fixed w-full top-0 left-0 z-20">
+            <Image src='/logo1.png' alt="logo" width={200} height={200} />
+            <div className="hidden md:flex border p-2 rounded-lg bg-gray-200 w-96">
+                <input type="text" className="w-full bg-transparent outline-none" />
+                <Search />
+
+            </div>
+            {
+                isSignedIn ? <UserButton /> : <div className="flex gap-5">
+                    <SignInButton mode="modal">
+                        <Button variant="outline">Login</Button>
+                    </SignInButton>
+                    <SignUpButton mode="modal">
+                        <Button>Signup</Button>
+                    </SignUpButton>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Header
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import Provider from "./Provider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>
+          {children}
+        </Provider>
+        </body>
     </html>
     </ClerkProvider>
   );
